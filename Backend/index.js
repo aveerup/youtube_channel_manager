787bc21e86d19d939b00e9edfdbf701f677459b4
aveerup <achy122@gmail.com>
2025-09-20 02:35:36 +0600

    used GOOGLE_CALLBACK_URL in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -77,7 +77,7 @@ app.use((req, res, next) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3001/auth/google/callback'
 }, async (accessToken, refreshToken, params, profile, done) => {
   const user = {
     id: profile.id,
